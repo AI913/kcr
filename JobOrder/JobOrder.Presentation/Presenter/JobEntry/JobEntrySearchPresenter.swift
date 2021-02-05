a2d9264bcea5709dec207435a7fd4bb0a22b4808
ako <>
2021-02-05 16:23:59 +0900

    no message

--- JobOrder/JobOrder.Presentation/Presenter/JobEntry/JobEntrySearchPresenter.swift
+++
@@ -1,9 +0,0 @@
-//
-//  JobEntrySearchPresenter.swift
-//  JobOrder.Presentation
-//
-//  Created by Frontarc on 2021/02/05.
-//  Copyright © 2021 Kento Tatsumi. All rights reserved.
-//
-
-import Foundation
