0057e395319b1b5d8331919ed1cda57fdd66c901
ako <>
2021-02-05 16:23:39 +0900

    no message

---
+++ JobOrder/JobOrder.Presentation/Presenter/JobEntry/JobEntrySearchPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  JobEntrySearchPresenter.swift
+//  JobOrder.Presentation
+//
+//  Created by Frontarc on 2021/02/05.
+//  Copyright © 2021 Kento Tatsumi. All rights reserved.
+//
+
+import Foundation
---
+++ JobOrder/JobOrder.Presentation/ViewController/JobEntry/JobEntrySearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JobEntrySearchViewController.swift
+//  JobOrder.Presentation
+//
+//  Created by Frontarc on 2021/02/05.
+//  Copyright © 2021 Kento Tatsumi. All rights reserved.
+//
+
+import UIKit
+
+class JobEntrySearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
